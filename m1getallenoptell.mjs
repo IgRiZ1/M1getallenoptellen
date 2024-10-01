275ff35d00c350af962a2759de78ddc9de0da7c1
IgRiZ1 <dalil.blh@outlook.be>
2024-10-01 10:32:13 +0200

    Create m1getallenoptell.mjs

---
+++ m1getallenoptell.mjs
@@ -0,0 +1,31 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+import { get } from 'node:https';
+import { log } from 'node:console';
+const userInput = readline.createInterface({input, output});
+
+let getal;
+do{
+    getal = parseFloat(await userInput.question('geef een getal? '))
+    let som = getal + getal
+    console.log(som);
+    
+}while(getal != 0){
+    console.log('ik wil niet nul berekenen')
+}
+
+
+ process.exit()
+    
+   
+    
+   //-- while( j < 0){
+  //  let result = i + j;
+       // console.log(`${i} + ${j} = ${result}`);
+       // j++;
+   // }
+    //console.log("-----------------");
+   // i++;
+
+
+process.exit()
